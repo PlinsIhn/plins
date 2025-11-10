206ba7752bc65abee2e3126e100a7fcdc4398ff9
PlinsIhn <hrpirkt@gmail.com>
2025-11-10 15:24:07 +0200

    1 preparation avant deployer

--- plins/settings.py
+++ plins/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gb21lz)pizj0ftoox+93!+oy&gab__m1un%_q4jc8%kfie#qn_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 # Application definition
